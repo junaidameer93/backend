9ab38322c18204e3e8798a83bce0378077b0e852
junaidameer93 <junaid.ameer93@gmail.com>
2025-01-19 22:33:08 +0500

    db connection with mongo atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//require("dotenv").config({path: '/.env'});
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({path: '/.env'}); 
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app = express();
+
+// (async()=>{
+//     try {
+//         const db = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.error("error",error);
+//             throw error
+//         });
+//         app.listen(process.env.PORT , () => console.log(`Server is running on port ${process.env.PORT}`));    
+        
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })()
